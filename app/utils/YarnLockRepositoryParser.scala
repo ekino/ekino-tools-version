d38ec7ffdc803a54e49379f53f151bac5f3e748c
philippe.agra <philippe.agra@ekino.com>
2019-08-29 11:09:10 +0200

    [BUGFIX] choosing the higher version when a dependency is present several times on yarn.lock

--- app/utils/YarnLockRepositoryParser.scala
+++ app/utils/YarnLockRepositoryParser.scala
@@ -12,7 +12,9 @@ import scala.util.matching.Regex
 object YarnLockRepositoryParser extends AbstractParser {
 
   private val buildFileName = "yarn.lock"
-  private val yarnVersion = """"?([^\n,]*)@.*:\n\s+version "(.*)"""".r
+  private val yarnVersion = """"?([^\n,]*)@(.*):\n\s+version "(.*)"""".r
+  private val versionValue = """[.\da-zA-Z-]+""".r
+  val extractYarnDependency: ExtractGroups[YarnDependency] = matchData => matchData.group(1) -> YarnDependency(matchData.group(3), matchData.group(2))
 
   override def buildRepository(folder: File, groupName: String, springBootDefaultData: SpringBootData, springBootMasterData: SpringBootData): Repository = {
     // project files
@@ -28,16 +30,23 @@ object YarnLockRepositoryParser extends AbstractParser {
   private def getDependencies(buildFile: File, folder: File, groupName: String): Seq[Dependency] = {
     val subfolder = getSubfolder(buildFile, folder)
 
-    val npmArtifacts = NPMRepositoryParser.buildRepository(buildFile.getParentFile, groupName, SpringBootData.noData, SpringBootData.noData)
+    val npmArtifacts: Seq[Dependency] = NPMRepositoryParser.buildRepository(buildFile.getParentFile, groupName, SpringBootData.noData, SpringBootData.noData)
       .dependencies
-      .map(_.name)
 
-    extractFromFile(buildFile, yarnVersion, extractProperties)
-      .filter(p => npmArtifacts.contains(p._1))
-      .map(p => NodeDependency(p._1, p._2, subfolder))
+    extractFromFile(buildFile, yarnVersion, extractYarnDependency)
+      .filter(p => npmArtifacts.exists(dep => dep.name == p._1 && containsVersion(p._2.semver, dep.version)))
+      .map(p => NodeDependency(p._1, p._2.version, subfolder))
       .toSeq
   }
 
   override def getBuildFilesRegex: Regex = buildFileName.r
+
+  def containsVersion(line: String, version: String): Boolean =
+    versionValue.findAllIn(line)
+      .matchData
+      .map(_.matched)
+      .contains(version)
 }
+
+case class YarnDependency(version: String, semver: String)
 
